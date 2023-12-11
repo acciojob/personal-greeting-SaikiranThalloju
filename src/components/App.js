c69effae99dab0186506a78a92428e51e36d3805
Saikiran Thalloju <saikiranthalloju008@gmail.com>
2023-12-11 19:33:25 +0530

    submitting code-Mon Dec 11 2023 19:33:22 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 
-import React ,{useState}from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-
-
 const App = () => {
-  const [text,setText] = useState("")
+  const [text, setText] = useState("");
 
-function inputText (e) {
-  setText(e.target.value);
-  console.log(setText);
-}
-
+  function inputText(e) {
+    setText(e.target.value);
+  }
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <input type="text" 
-        onChange={inputText} />
-<p>{text}!</p>
+      {/* Do not remove the main div */}
+      <input type="text" onChange={(e) => inputText(e)} />
+      {text && <p>Hello{text}!</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
